381dcd6b104483520fe054237469e56e9bcaab61
MGF15 <mohamed5548@live.com>
2015-11-09 02:53:49 -0800

    Update SIGMA QR-Code.py

--- SIGMA QR-Code.py
+++ SIGMA QR-Code.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-#SIGMA QR-Code (URL + Text)
+#SIGMA QR-Code 
 #Coded by MGF15
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import os 
-import pyqrcode
+import os , pyqrcode 
+from urllib import unquote
 
 PORT_NUMBER = 8080
 Dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
@@ -31,7 +31,8 @@ class myHandler(BaseHTTPRequestHandler):
 				self.send_response(200)
 				self.send_header('Content-type',type)
 				self.end_headers()
-				self.wfile.write(f.read())
+				html = f.read()
+				self.wfile.write(html)
 				f.close()
 			return
 
@@ -39,17 +40,40 @@ class myHandler(BaseHTTPRequestHandler):
 			self.send_error(404,'File Not Found: %s' % self.path)
 	def do_POST(self):
 		content_length = int(self.headers['Content-Length'])
-		file_content = self.rfile.read(content_length)
-		g = file_content.split('url=')
-		w = g[1].replace('%3A',':').replace('%2F','/')
-		qr = pyqrcode.create(w)
-		qr.svg('qr.svg', scale=9)
+		t = self.rfile.read(content_length)
+		v = unquote(t).decode('utf8')
+		if v.split('=')[0] == 'Link' :
+			Qr = v.split('=')[1]
+			qr = pyqrcode.create(Qr)
+			qr.svg('qr-code.svg', scale=9)
+		if v.split('=')[0] == 'Play' :
+			Qr = v.split('Play=')[1]
+			qr = pyqrcode.create(Qr)
+			qr.svg('qr-code.svg', scale=7)
+		if v.split('=')[0] == 'Text' :
+			Qr = v.split('=')[1]
+			qr = pyqrcode.create(Qr)
+			qr.svg('qr-code.svg', scale=9)
+		if v.split('=')[0] == 'Tel' :
+			Qr = v.split('=')[1]
+			qr = pyqrcode.create('TEL:'+Qr)
+			qr.svg('qr-code.svg', scale=9)
+		if v.split('=')[0] == 'Sms':
+			sms = v.replace('&','=').split('=')
+			SMs = 'SMSTO:'+sms[1]+':'+sms[3].replace('+',' ')
+			qr = pyqrcode.create(SMs)
+			qr.svg('qr-code.svg', scale=9)
+		if v.split('=')[0] == 'BTC':
+			Qr = v.split('=')[1]
+			qr = pyqrcode.create(Qr)
+			qr.svg('qr-code.svg', scale=9)
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-		self.wfile.write(final)
+		s = final.replace('qr-code','qr-code.svg')
+		self.wfile.write(s)
 		
-server = HTTPServer(('', PORT_NUMBER), myHandler)
+server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
 print '[+] Open LocalHost in Your Browser on Port' , PORT_NUMBER
 print '\n[+] Started httpserver on port ' , PORT_NUMBER
 
